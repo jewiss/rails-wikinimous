d885d016bc4432b62f74e1a9ba7edf9b18914070
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-11 18:07:35 +0100

    exo 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+10.times do
+  article = Article.new(
+    title: Faker::FunnyName.name,
+    content: Faker::Superhero.name
+  )
+    article.save!
+end
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
